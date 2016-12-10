668c23cb81f4f96306bfcca85b4525d852d9cd62
Etienne Lachance <et@etiennelachance.com>
2016-12-10 14:51:53 -0500

    Update start script to fix some mysql connection issues

--- pdns/start.sh
+++ pdns/start.sh
@@ -5,17 +5,6 @@ mkdir -p /etc/powerdns/pdns.d
 PDNSVARS=`echo ${!PDNSCONF_*}`
 touch /etc/powerdns/pdns.conf
 
-if [ ! -z $MYSQL_ENV_MARIADB_DATABASE ]; then
-   PDNSCONF_GMYSQL_USER=$MYSQL_ENV_MARIADB_USER 
-   PDNSCONF_GMYSQL_DBNAME=$MYSQL_ENV_MARIADB_DATABASE 
-   PDNSCONF_GMYSQL_PASSWORD=$MYSQL_ENV_MARIADB_PASSWORD
-else 
-   PDNSCONF_GMYSQL_USER=$MYSQL_ENV_MYSQL_USER
-   PDNSCONF_GMYSQL_DBNAME=$MYSQL_ENV_MYSQL_DATABASE
-   PDNSCONF_GMYSQL_PASSWORD=$MYSQL_ENV_MYSQL_PASSWORD
-fi
-
-
 for var in $PDNSVARS; do
   varname=`echo ${var#"PDNSCONF_"} | awk '{print tolower($0)}' | sed 's/_/-/g'`
   value=`echo ${!var} | sed 's/^$\(.*\)/\1/'`
@@ -39,7 +28,7 @@ fi
 mysqlcheck() {
   # Wait for MySQL to be available...
   COUNTER=20
-  until mysql -h mysql -u $PDNSCONF_GMYSQL_USER -p$PDNSCONF_GMYSQL_PASSWORD -e "show databases" 2>/dev/null; do
+  until mysql -h $PDNSCONF_GMYSQL_HOST -u $PDNSCONF_GMYSQL_USER -p$PDNSCONF_GMYSQL_PASSWORD -e "show databases" 2>/dev/null; do
     echo "WARNING: MySQL still not up. Trying again..."
     sleep 10
     let COUNTER-=1
@@ -49,14 +38,14 @@ mysqlcheck() {
     fi
   done
 
-  count=`mysql -h mysql -u $PDNSCONF_GMYSQL_USER -p$PDNSCONF_GMYSQL_PASSWORD -e "select count(*) from information_schema.tables where table_type='BASE TABLE' and table_schema='$PDNSCONF_GMYSQL_DBNAME';" | tail -1`
+  count=`mysql -h $PDNSCONF_GMYSQL_HOST -u $PDNSCONF_GMYSQL_USER -p$PDNSCONF_GMYSQL_PASSWORD -e "select count(*) from information_schema.tables where table_type='BASE TABLE' and table_schema='$PDNSCONF_GMYSQL_DBNAME';" | tail -1`
   if [ "$count" == "0" ]; then
     echo "Database is empty. Importing PowerDNS schema..."
-    mysql -h mysql -u $PDNSCONF_GMYSQL_USER -p$PDNSCONF_GMYSQL_PASSWORD $PDNSCONF_GMYSQL_DBNAME < /usr/share/doc/pdns-backend-mysql/schema.mysql.sql && echo "Import done."
+    mysql -h $PDNSCONF_GMYSQL_HOST -u $PDNSCONF_GMYSQL_USER -p$PDNSCONF_GMYSQL_PASSWORD $PDNSCONF_GMYSQL_DBNAME < /usr/share/doc/pdns-backend-mysql/schema.mysql.sql && echo "Import done."
   fi
 }
 
-mysqlcheck
+#mysqlcheck
 
 if [ "$SECALLZONES_CRONJOB" == "yes" ]; then
   cat > /etc/crontab <<EOF
